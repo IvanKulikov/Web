7d926d5bd1eaf51f345fea3406955b6ee51ff4fb
IvanKulikov <167080565+IvanKulikov@users.noreply.github.com>
2024-04-24 15:16:15 +0300

    Update database.php

--- mvcsite/database.php
+++ mvcsite/database.php
@@ -1,17 +1,1 @@
-<?
 
-define('MYSQL_SERVER', 'mvcblog21');
-define('MYSQL_USER', 'root');
-define('MYSQL_PASSWORD', '');
-define('MYSQL_DB', 'mvcblog21');
-
-
-function db_connect(){
-  $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB)
-       or die("Error: " . mysqli_error($link));
-  if (!mysqli_set_charset($link, "utf8")){
-    printf("Error: " . mysqli_error($link));
-  }
-  return $link;
-}
-?>
